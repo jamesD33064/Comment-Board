0782210c79448e708999863ba230fe5d503731f8
黃威縉 <james.huang@program.com.tw>
2023-07-07 11:09:53 +0800

    #6 新增管理頁面

---
+++ public/js/page_manage.js
@@ -0,0 +1,32 @@
+const btn_del_comment = document.getElementById('btn_del_comment');
+sessionStorage.setItem("comment_del_id", '[]');
+var listItems = document.querySelectorAll('.single-comment');
+// 為每個 <li> 製作點擊事件
+listItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+        const _id = this.id;
+        var comment_del_id = JSON.parse(sessionStorage.getItem('comment_del_id'));
+        var newary = [];
+
+        if(this.classList.contains('active')){
+            this.classList.remove('active');
+            //移除特定字串
+            newary = comment_del_id.filter(function (letter) {
+                return letter !== _id;
+            });
+        } else {
+            this.classList.add('active');
+            comment_del_id.push(_id);         
+            newary = comment_del_id;
+        }
+        sessionStorage.setItem("comment_del_id", JSON.stringify(newary) );
+
+        //如果未選取任何留言
+        if (newary.length == 0) {
+            btn_del_comment.style.display = 'none';
+        } else {
+            btn_del_comment.style.display = 'block';
+        }
+        
+    });
+});
